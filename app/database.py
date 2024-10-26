7b89ba0a51e393e31ff6afbb1f33a8dc2ea34c16
sebastiankzk <40329724+sebastiankzk@users.noreply.github.com>
2024-10-26 21:10:51 +0800

    initial commit

---
+++ app/database.py
@@ -0,0 +1,38 @@
+import os 
+from os import getenv
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Get parameters from env file
+# load_dotenv(dotenv_path="../.env")
+
+# Read environment variables for database credentials
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+# Database URL
+# connectionString = getenv('DB_CONNECTION_STRING')
+connectionString: str = f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+
+# Check if connectionString is None
+if connectionString is None:
+    raise ValueError("DB_CONNECTION_STRING environment variable not set.")
+
+# SQLAlchemy Engine
+try:
+    engine = create_engine(connectionString, connect_args={"charset": "utf8mb4"})
+    # Optionally, test the connection
+    with engine.connect() as conn:
+        print("Connected to the database successfully.")
+except Exception as e:
+    raise ConnectionError(f"Failed to connect to the database: {e}")
+
+# Session Local
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base
+Base: DeclarativeMeta = declarative_base()
---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+# from app.routes.vendor_router import router as vendor_router
+from mangum import Mangum
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# app.include_router(vendor_router)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+handler = Mangum(app=app)
